63b7556ebb5cb27a54661813209ddf99af2e2840
bull0n <lucas.bulloni@protonmail.ch>
2018-08-23 10:46:04 +0200

    forgot the view class in the last commit

---
+++ views/tab-content/view-class.js
@@ -0,0 +1,28 @@
+class View
+{
+  constructor(name, icon)
+  {
+    this.name = name;
+    this.icon = icon;
+  }
+
+  getButtonDisplay()
+  {
+    return `<div id="${this.getIdButton()}" class="btn-content btn-view"><p>${this.name.charAt(0).toUpperCase()}</p></div>`;
+  }
+
+  getContentDiv()
+  {
+    return `<div id="${this.getIdContentDiv()}" class="view"></div>`;
+  }
+
+  getIdContentDiv()
+  {
+    return this.name.toLowerCase() + '-view';
+  }
+
+  getIdButton()
+  {
+    return this.name.toLowerCase() + '-btn';
+  }
+}
