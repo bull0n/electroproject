180c547694635721969ca165cd0f84a1ebd161a0
bull0n <lucas.bulloni@protonmail.ch>
2018-08-23 11:03:34 +0200

    forgot class file in last function

---
+++ views/tab-content/btn-action-class.js
@@ -0,0 +1,18 @@
+class ButtonAction
+{
+  constructor(name, icon)
+  {
+    this.name = name;
+    this.icon = icon;
+  }
+
+  getButtonDisplay()
+  {
+    return `<div id="${this.getIdButton()}" class="btn-content btn-action"><p>${this.name.charAt(0).toUpperCase()}</p></div>`;
+  }
+
+  getIdButton()
+  {
+    return this.name.toLowerCase() + '-btn';
+  }
+}
