1c1326397b5e233126832b24291e14dc56a98aa4
Malik Fleury <malik.fleury@he-arc.ch>
2018-08-28 14:48:18 +0200

    Added modals: add member, modify member, remove member

--- views/list-members/list-members.js
+++ views/list-members/list-members.js
@@ -9,8 +9,7 @@ class ListMembers extends AbstractView
   {
     super(element);
     this.project = project;
-    this.listMembers = this.project.team;
-    this.listMembers = SerializerTool.unserializeFromFile("./team.epd");  // Tests
+    this.project.team = SerializerTool.unserializeFromFile("./team.epd");  // Tests
   }
 
   display()
@@ -21,10 +20,12 @@ class ListMembers extends AbstractView
     <table id="tbl_members" class="table table-hover">
       <col id="tbl_column_color">
       <col id="tbl_column_name">
+      <col id="tbl_column_Controls">
       <thead>
         <tr>
           <th scope="col">Color</th>
           <th scope="col">Name</th>
+          <th scope="col">Controls</th>
         </tr>
       </thead>
       <tbody>
@@ -34,10 +35,8 @@ class ListMembers extends AbstractView
       </tbody>
     </table>
 
-    <div id="div-team-edition" class="btn-group" role="group">
-      <button type="button" class="btn btn-primary btn-team-edition" id="btn-add-member">Add</button>
-      <button type="button" class="btn btn-primary btn-team-edition" id="btn-modify-member">Modify</button>
-      <button type="button" class="btn btn-primary btn-team-edition" id="btn-remove-diagram">Remove</button>
+    <div class="btn-team-edition" class="btn-group" role="group">
+      <button type="button" id="${this.project.name.toLowerCase()}-add-member" class="btn btn-primary"><i class="fas fa-plus"></i> Add a member</button>
     </div>
     `;
 
@@ -45,10 +44,18 @@ class ListMembers extends AbstractView
     this.addEvent();
   }
 
-  addMember(member)
+  addHTMLRowMember(member)
   {
-    let htmlText = `<tr><td bgcolor="` + member.color + `"></td>`;
-    htmlText += `<td>` + member.name + `</td></tr>`;
+    let htmlText = `
+    <tr>
+      <td bgcolor="` + member.color + `"></td>
+      <td>` + member.name + `</td>
+      <td>
+        <button class="btn btn-link btn-modify-member" data-task-id=""><i class="fas fa-edit" data-task=""></i></button>
+        <button class="btn btn-link btn-remove-member" data-task-id=""><i class="fas fa-trash" data-task=""></i></button>
+      </td>
+    </tr>`;
+
     return htmlText;
   }
 
@@ -56,10 +63,10 @@ class ListMembers extends AbstractView
   {
     let htmlText = ``;
 
-    for(let i = 0;i < this.listMembers.length; i++)
+    for(let i = 0;i < this.project.team.length; i++)
     {
-      let member = this.listMembers[i];
-      htmlText += this.addMember(member);
+      let member = this.project.team[i];
+      htmlText += this.addHTMLRowMember(member);
     }
 
     return htmlText;
@@ -67,11 +74,87 @@ class ListMembers extends AbstractView
 
   addEvent()
   {
-    // Actions on the page
-    $(document).ready(function()
-    {
+    let project = this.project;
+    let listMembers = this;
 
-    });
+    let addMemberClickEvent = function(event)
+    {
+      let ConfirmModal = require('../confirm-modal/confirm-modal.js');
+
+      ConfirmModal.show('Add member', listMembers.getHTMLFormFunc("#345321", "coco"), function() { listMembers.addMemberConfirm(); });
+    };
+
+    $(`#${this.project.name.toLowerCase()}-add-member`).click(addMemberClickEvent);
+
+    let modifyMemberClickEvent = function(event)
+    {
+      let ConfirmModal = require('../confirm-modal/confirm-modal.js');
+      let index = $(this).closest("tr").index();
+      let member = project.team[index];
+
+      ConfirmModal.show('Modify member', listMembers.getHTMLFormFunc(member.color, member.name), function() { listMembers.modifyMemberConfirm(member); });
+    };
+
+    $(`.btn-modify-member`).click(modifyMemberClickEvent);
+
+    let removeMemberClickEvent = function(event)
+    {
+      let ConfirmModal = require('../confirm-modal/confirm-modal.js');
+      let index = $(this).closest("tr").index();
+
+      ConfirmModal.show('Modify member', listMembers.getHTMLRemoveMemberWarningMessage(), function() { listMembers.removeMemberConfirm(index); });
+    };
+
+    $(`.btn-remove-member`).click(removeMemberClickEvent);
+  }
+
+  addMemberConfirm()
+  {
+    let color = $('#cp_member_color').val();
+    let name = $('#txt_member_name').val();
+
+    let member = new Member();
+    member.color = color;
+    member.name = name;
+    this.project.team.push(member);
+
+    this.display();
+  }
+
+  modifyMemberConfirm(member)
+  {
+    let color = $('#cp_member_color').val();
+    let name = $('#txt_member_name').val();
+
+    member.color = color;
+    member.name = name;
+
+    this.display();
+  }
+
+  removeMemberConfirm(index)
+  {
+    this.project.team.splice(index, 1);
+
+    this.display();
+  }
+
+  getHTMLFormFunc(color, name)
+  {
+    let htmlText = `
+    <label for="color_picker">Color</label>
+    <input type="color" id="cp_member_color" value="` + color + `" name="textcolor"></input>
+    <br>
+    <label for="txt_name">Name</label>
+    <input type="text" id="txt_member_name" name="name" value="` + name + `"></input><br>
+    `;
+
+    return htmlText;
+  }
+
+  getHTMLRemoveMemberWarningMessage()
+  {
+    return 'Are you sure to remove this member ?';
   }
 }
 
