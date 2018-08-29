eda48a55db69581bb84a0bf35158342a71dfd84f
Malik Fleury <malik.fleury@he-arc.ch>
2018-08-29 13:26:36 +0200

    Added function to load file

--- views/file-dialog/file-dialog.js
+++ views/file-dialog/file-dialog.js
@@ -1,17 +1,39 @@
 
 const {dialog} = require('electron').remote;
 let SerializerTool = require('../../tools/serializertool.js');
+let Project = require("../../data/project.js");
 
 class FileDialog
 {
-  static saveAs(project, directory)
+  static saveAs(project, directory, parentWindow = null)
   {
-    let path = dialog.showSaveDialog(window.win, {title:"Save as", defaultPath:directory});
+    let path = dialog.showSaveDialog(parentWindow, {title:"Save as", defaultPath:directory});
 
     if(path)
     {
       SerializerTool.serializeToFile(project, path);
     }
+    else
+    {
+      throw "The user canceled the saving...";
+    }
+  }
+
+  static open(directory, parentWindow = null)
+  {
+    let path = dialog.showOpenDialog(parentWindow, {title:"Open", defaultPath:directory});
+    let project = null;
+
+    if(path)
+    {
+      project = SerializerTool.unserializeFromFile(path);
+    }
+    else
+    {
+        throw "The user canceled the loading...";
+    }
+
+    return project;
   }
 }
 
