5cc1a727e59a949f2234a53553d6c1848df5be97
bull0n <lucas.bulloni@protonmail.ch>
2018-08-29 15:06:45 +0200

    display the gantt with divs

--- views/diagram-view/diagram-view.js
+++ views/diagram-view/diagram-view.js
@@ -1,5 +1,7 @@
 let AbstractView = require('../../abstract-view-class.js');
 
+const MS_IN_DAYS = 86400000;
+
 class DiagramView extends AbstractView
 {
   constructor(element, project, prefix)
@@ -7,6 +9,7 @@ class DiagramView extends AbstractView
     super(element);
     this.project = project;
     this.prefix = prefix;
+    this.WIDTH_DAY = 30;
   }
 
   display()
@@ -18,44 +21,35 @@ class DiagramView extends AbstractView
         <div class="separator-container">
           ${ this.buildSeparator() }
         </div>
-        <div class="row-container">
-          <div class="row-planning title-planning" id="${this.prefix}-taskname">
-            <div class="task-name planning-label">
-              Task name
-            </div>
-            <div class="working-on planning-label">
-              People working on it
-            </div>
+        <div class="row-planning title-planning task" id="${this.prefix}-taskname">
+          <div class="task-name planning-label planning-title">
+            Task name
           </div>
-          <div class="row-planning" id="${this.prefix}-taskname">
-            <div class="task-name planning-label">
-              Tâche 1
-            </div>
-            <div class="working-on planning-label">
-              lucas, malik
-            </div>
-            <div class="period-container"><div class="period"></div></div>
+          <div class="in-charge planning-label task-name planning-title">
+            Person in charge
           </div>
+        </div>
 
-          <div class="row-planning">
-            <button class="btn btn-primary btn-action-diagram" id="${this.prefix}-add-task-diagram"><i class="fas fa-plus"></i> Add a task</button>
-          </div>
-          <div class="row-planning">
-            <button class="btn btn-primary btn-action-diagram" id="${this.prefix}-no-sort">Original order</button>
-          </div>
-          <div class="row-planning">
-            <button class="btn btn-primary btn-action-diagram" id="${this.prefix}-member-sort">Sort by member</button>
-          </div>
-          <div class="row-planning">
-            <div class="btn-group">
-              <button type="button" class="btn btn-primary dropdown-toggle btn-action-diagram" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Member to show
-              </button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
+        ${this.buildTasks(undefined)}
+
+        <div class="row-planning">
+          <button class="btn btn-primary btn-action-diagram" id="${this.prefix}-add-task-diagram"><i class="fas fa-plus"></i> Add a task</button>
+        </div>
+        <div class="row-planning">
+          <button class="btn btn-primary btn-action-diagram" id="${this.prefix}-no-sort">Original order</button>
+        </div>
+        <div class="row-planning">
+          <button class="btn btn-primary btn-action-diagram" id="${this.prefix}-member-sort">Sort by member</button>
+        </div>
+        <div class="row-planning">
+          <div class="btn-group btn-dropdown-planning">
+            <button type="button" class="btn btn-primary dropdown-toggle btn-action-diagram" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Member to show
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
             </div>
           </div>
         </div>
@@ -63,28 +57,42 @@ class DiagramView extends AbstractView
     `;
 
     $(this.element).html(htmlText);
-    this.buildTasks();
   }
 
   buildTasks(order)
   {
+    let htmlTasks = '';
+
     for(let i = 0; i < this.project.tasks.length; i++)
     {
       let task = this.project.tasks[i];
+      let daysFromStart = Math.floor((task.from - this.project.from()) / MS_IN_DAYS);
 
-      $(`${this.prefix}-list-tasks`).append(`<li class="list-group-item">Cras justo odio</li>`)
+      htmlTasks += `<div class="row-planning task" id="${this.prefix}-${task.name.toLowerCase()}">
+        <div class="task-name planning-label">
+          ${task.name.toLowerCase()}
+        </div>
+        <div class="in-charge planning-label">
+          ${task.inCharge !== undefined ? task.inCharge.name : ''}
+        </div>
+        <div class="period-container">
+          <div class="period" style="width: ${(task.getLengthInDays()+1) * this.WIDTH_DAY}px; margin-left: ${daysFromStart * this.WIDTH_DAY}px; background-color: ${task.inCharge !== undefined ? task.inCharge.color : 'DeepSkyBlue'}"></div>
+        </div>
+      </div>`;
     }
+
+    return htmlTasks;
   }
 
   buildSeparator()
   {
-    let nDays = Math.floor((this.project.to() - this.project.from())/86400000); // transform into days
+    let nDays = Math.floor((this.project.to() - this.project.from()) / MS_IN_DAYS); // transform into days
 
     let html = '';
 
-    for(let i = 0; i < nDays; i++)
+    for(let i = 0; i <= nDays; i++)
     {
-      html += '<div class="separator-planning"></div>';
+      html += '<div class="separator-planning" style="width: '+this.WIDTH_DAY+'px"></div>';
     }
 
     return html;
