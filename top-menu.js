d1b034dfef4b0887f08fd5a098cc2edb16236ca9
Malik Fleury <malik.fleury@he-arc.ch>
2018-09-03 10:50:04 +0200

    Moved some function from home to top-menu

--- top-menu.js
+++ top-menu.js
@@ -1,6 +1,4 @@
-const {Menu} = require('electron')
-const electron = require('electron')
-const app = electron.app
+const FileDialog = require("./views/file-dialog/file-dialog.js");
 
 class TopMenu
 {
@@ -14,7 +12,7 @@ class TopMenu
             accelerator: 'Ctrl+n',
             click (item, focusedWindow)
             {
-              console.log("NEW");
+              TopMenu.newProject();
             }
           },
           {
@@ -22,18 +20,76 @@ class TopMenu
             accelerator: 'Ctrl+o',
             click (item, focusedWindow)
             {
-              console.log("OPEN");
+              TopMenu.openProject();
             }
           },
           {
             label: 'Quit',
-            role: 'quit'
+            role: 'Ctrl+q',
+            click (item, focusedWindow)
+            {
+              TopMenu.quitApp();
+            }
           }
         ]
-      }];
+      },
+      {
+        label: 'Dev',
+        submenu: [
+          {
+            role: 'reload'
+          },
+          {
+            role: 'toggledevtools'
+          }
+        ]
+      }
+      ];
 
       let menu = Menu.buildFromTemplate(template)
       Menu.setApplicationMenu(menu)
+  }
+
+
+  static newProject()
+  {
+    let tabView = TabView.getInstance();
+    let project = new Project();
+
+    Modal.show('New project', TopMenu.getHTMLProjectNameForm(), function()
+    {
+      project.name = $('#txt_project_name').val();
+
+      tabView.display();
+      tabView.createTab(project);
+    });
+  }
+
+  static openProject()
+  {
+    let directory = app.getPath('documents');
+    let project = FileDialog.open(directory, BrowserWindow.getFocusedWindow());
+    let tabView = TabView.getInstance();
+
+    tabView.display();
+    tabView.createTab(project);
+  }
+
+  static quitApp()
+  {
+    app.quit();
+  }
+
+  static getHTMLProjectNameForm()
+  {
+    let htmlText = `
+    <div class="form-group">
+      <label for="txt_name">Name</label>
+      <input type="text" id="txt_project_name" name="name" value="${name}" class="form-control" placeholder="name">
+    </div>
+    `;
+
+    return htmlText;
   }
 }
 
--- views/home/home.js
+++ views/home/home.js
@@ -1,13 +1,5 @@
-/*
-$('#btn-new-diagram, #btn-open-diagram').click(function()
-{
-  $('body').load('views/tab-view/tab-view.html');
-})
-*/
-
+let SerializerTool = require('../../tools/serializertool.js');
 let AbstractView = require("../../abstract-view-class.js");
-let FileDialog = require("../file-dialog/file-dialog.js");
-let TabView = require('../tab-view/tab-view.js');
 let Project = require('../../data/project.js').Project;
 
 class Home extends AbstractView
@@ -40,58 +32,9 @@ class Home extends AbstractView
     let openProjectClickEvent = this.openProject;
     let quitAppClickEvent = this.quitApp;
 
-    $('#btn-new-project').click({source:this}, newProjectClickEvent);
-    $('#btn-open-project').click({source:this}, openProjectClickEvent);
-    $('#btn-quit-app').click({source:this}, quitAppClickEvent);
-  }
-
-  newProject(event)
-  {
-    let tabView = new TabView($('#content-container'));
-    let project = new Project();
-    let ConfirmModal = require('../modal/modal.js');
-    let source = event.data.source;
-
-    ConfirmModal.show('New project', source.getHTMLProjectNameForm(), function()
-    {
-      let tabView = new TabView($('#content-container'));
-
-      project.name = $('#txt_project_name').val();
-
-      source.display();
-      tabView.display();
-      tabView.createTab(project);
-    });
-  }
-
-  openProject(event)
-  {
-    const {app, BrowserWindow} = require('electron').remote;
-
-    let directory = app.getPath('documents');
-    let project = FileDialog.open(directory, BrowserWindow.getFocusedWindow());
-    let tabView = new TabView($('#content-container'));
-
-    tabView.display();
-    tabView.createTab(project);
-  }
-
-  quitApp(event)
-  {
-    const {app} = require('electron').remote;
-    app.quit();
-  }
-
-  getHTMLProjectNameForm()
-  {
-    let htmlText = `
-    <div class="form-group">
-      <label for="txt_name">Name</label>
-      <input type="text" id="txt_project_name" name="name" value="${name}" class="form-control" placeholder="name">
-    </div>
-    `;
-
-    return htmlText;
+    $('#btn-new-project').click(TopMenu.newProject);
+    $('#btn-open-project').click(TopMenu.openProject);
+    $('#btn-quit-app').click(TopMenu.quitApp);
   }
 }
 
