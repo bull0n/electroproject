0bd2e5ffd574b9bfc99448c1cd2c05ba0314d58b
bull0n <lucas.bulloni@protonmail.ch>
2018-09-04 09:18:27 +0200

    conflict

--- views/home/home.js
+++ views/home/home.js
@@ -32,64 +32,9 @@ class Home extends AbstractView
     let openProjectClickEvent = this.openProject;
     let quitAppClickEvent = this.quitApp;
 
-<<<<<<< HEAD
     $('#btn-new-project').click(topMenu.newProject);
     $('#btn-open-project').click(topMenu.openProject);
     $('#btn-quit-app').click(topMenu.quitApp);
-=======
-    $('#btn-new-project').click({source:this}, newProjectClickEvent);
-    $('#btn-open-project').click({source:this}, openProjectClickEvent);
-    $('#btn-quit-app').click({source:this}, quitAppClickEvent);
-  }
-
-  newProject(event)
-  {
-    let tabView = new TabView($('#content-container'));
-    let project = new Project();
-    let Modal = require('../modal/modal.js');
-    let source = event.data.source;
-
-    Modal.show('New project', source.getHTMLProjectNameForm(), function()
-    {
-      let tabView = new TabView($('#content-container'));
-
-      project.name = $('#txt_project_name').val();
-
-      source.display();
-      tabView.display();
-      tabView.createTab(project);
-    });
-  }
-
-  openProject(event)
-  {
-    const {app, BrowserWindow} = require('electron').remote;
-
-    let directory = app.getPath('documents');
-    let project = FileDialog.open(directory, BrowserWindow.getFocusedWindow());
-    let tabView = new TabView($('#content-container'));
-
-    tabView.display();
-    tabView.createTab(project);
-  }
-
-  quitApp(event)
-  {
-    const {app} = require('electron').remote;
-    app.quit();
-  }
-
-  getHTMLProjectNameForm()
-  {
-    let htmlText = `
-    <div class="form-group">
-      <label for="txt_name">Name</label>
-      <input type="text" id="txt_project_name" name="name" value="${name}" class="form-control" placeholder="name">
-    </div>
-    `;
-
-    return htmlText;
->>>>>>> diagram-view
   }
 }
 
